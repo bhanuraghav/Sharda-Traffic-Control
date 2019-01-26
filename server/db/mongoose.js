45a809e5efda4fea1d9bede3a96d6805f5e385db
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-27 01:24:02 +0530

    mlab added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ const config = require('./../../config/config')
 
 mongoose.Promise = global.Promise
 console.log(config.MONGO_URI)
-mongoose.connect(config.MONGO_URI || process.env.MONGO_URI,{ useNewUrlParser: true })
+// mongoose.connect(config.MONGO_URI || process.env.MONGO_URI,{ useNewUrlParser: true })
 
+mongoose.connect('mongodb://trafficcontrol:raghav12@ds113765.mlab.com:13765/traffic')
 module.exports = {mongoose}
