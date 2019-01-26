395fe353d5edafc7a3c4fcf7b37c9f86adc79575
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-26 13:50:47 +0530

    First Commit

---
+++ server/app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path')
+const session = require('express-session')
+const passport = require('passport');
+const flash = require("connect-flash");
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const methodOverride = require("method-override");
+
+const config = require('../config/config')
+const PORT = process.env.PORT || 3000 ;
+
+const app = express();
+
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(flash()); 
+app.use(methodOverride("_method"));
+
+app.use(session({
+    secret: process.env.PASSPORT_SECRET || config.PASSPORT_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.set('views',path.join(__dirname,'../views'))
+app.set('view engine','ejs');
+
+
+app.use(function(req,res,next){
+	res.locals.user = req.user;
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+app.use('/',routes);
+app.use(express.static(path.join(__dirname,'../public')))
+
+app.listen(PORT,console.log(`Server started on port ${PORT}`))
